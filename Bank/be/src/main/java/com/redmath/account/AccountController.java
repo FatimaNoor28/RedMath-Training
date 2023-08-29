a5a2509a1d34ec302600b050608a0891bee87e9f
FatimaNoor28 <108144307+FatimaNoor28@users.noreply.github.com>
2023-08-29 21:38:24 +0500

    some changes in assignment

--- Bank/be/src/main/java/com/redmath/account/AccountController.java
+++ Bank/be/src/main/java/com/redmath/account/AccountController.java
@@ -1,6 +1,7 @@
 package com.redmath.account;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,18 +11,19 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/v1/bank/account")
+@RequestMapping("/api/v1/myBank/account")
 public class AccountController {
     private final AccountService service;
 
     public AccountController(AccountService service){ this.service = service; }
 
     @RequestMapping()
-    //@PreAuthorize("hasAuthority('ADMIN')")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public ResponseEntity<List<Account>> findAll(){ return ResponseEntity.ok(service.findAll()); }
 
 
     @RequestMapping("/{id}")
+    @PreAuthorize("hasAuthority('ACCOUNT_HOLDER')")
     public ResponseEntity<Account> findById(@PathVariable("id") Long id){
         Optional<Account> account = service.findById(id);
         if(account.isEmpty()){ResponseEntity.notFound().build(); }
