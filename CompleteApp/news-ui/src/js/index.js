510aba921b74be50a172982a52297bd10cbb4b1c
FatimaNoor28 <108144307+FatimaNoor28@users.noreply.github.com>
2023-08-30 22:33:45 +0500

    session 8

--- CompleteApp/news-ui/src/js/index.js
+++ CompleteApp/news-ui/src/js/index.js
@@ -1,4 +1,19 @@
 (function () {
     'use strict';
-    document.getElementById("p123").innerHTML = new Date();
+    function NewsConfig($locationProvider, $routeProvider ){
+        $locationProvider.hashPrefix('!');
+        $routeProvider.when('/news',{
+             templateUrl: 'news/news.html',
+             controller: 'NewsController as $ctrl'
+        }).when('/',{
+              templateUrl: 'hello-world.html',
+
+//              templateUrl: 'news/news.html',
+//              controller: 'NewsController as $ctrl'
+        }).otherwise('/');
+    }
+    angular.module('news-fe', ['ngRoute', 'ngResource', 'ng'])
+    .config(['$locationProvider', '$routeProvider', 'NewsConfig'])
+
+
 }());
